2568934ea323070bb7fb9eeab08acf14bc86875b
Mahdi <mtzarrin@gmail.com>
2024-09-25 13:18:50 +0330

    create mini rep

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,83 @@
-use chrono::{DateTime, Local, NaiveDateTime, Utc};
-use leptos::{leptos_dom::logging::console_log, *};
-use leptos_use::{storage::use_local_storage, utils::JsonCodec};
+use chrono::{DateTime, Local};
+use leptos::{either::Either, prelude::*};
+use logging::console_log;
+use reactive_stores::{OptionStoreExt as _, Store, StoreFieldIterator};
+use reactive_stores_macro::Store;
 use serde::{Deserialize, Serialize};
 use web_sys::MouseEvent;
+
 fn main() {
   console_log::init_with_level(log::Level::Debug).unwrap_or_default();
   console_error_panic_hook::set_once();
 
-  leptos::mount_to_body(App);
+  mount_to_body(App);
 }
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
+
+#[derive(Store, Clone, Debug, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct Note {
-  pub title: RwSignal<String>,
-  pub content: RwSignal<String>,
+  pub title: String,
+  pub content: String,
   pub date: DateTime<Local>,
 }
 
-pub struct AppModel {}
-impl AppModel {}
 #[component]
 fn App() -> impl IntoView {
-  let (notes, set_notes, _) = use_local_storage::<Vec<Note>, JsonCodec>("notes");
-  let (selected_note, set_selected_note) = create_signal(<Option<Note>>::None);
-  if notes.with(|f| f.is_empty() == false) {
-    set_selected_note.set(Some(notes.with(|f| f[0])));
+  let notes = Store::new(Vec::<Note>::new());
+  let selected_note = Store::new(<Option<Note>>::None);
+  if let Some(first_note) = notes.read().first() {
+    selected_note.set(Some(first_note.to_owned()));
   }
   let add_notes = move |_| {
-    set_notes.update(|f| {
-      f.insert(0, Note { date: Local::now(), title: create_rw_signal("Title".to_string()), content: create_rw_signal("Content".to_string()) })
-    });
-    set_selected_note.set(Some(notes.with(|f| f[0])));
+    let new_note =
+      Note { date: Local::now(), title: "Title".to_string(), content: "Content".to_string() };
+    notes.write().insert(0, new_note.clone());
+    console_log(&format!("{:#?}", notes.iter().map(|it| it.date().get()).collect::<Vec<_>>()));
+    console_log(&format!("{:#?}", notes.at(0).get()));
+    selected_note.set(Some(new_note));
   };
-
   let delete_note = move |child: Note| {
     move |event: MouseEvent| {
       event.stop_propagation();
-      if notes.with(|f| f.len() == 1) {
-        set_selected_note.set(None);
-      } else if notes.with(|f| f.last().unwrap().date != child.date) {
-        set_selected_note.set(Some(notes.with(|f| {
-          let pos = f.iter().position(|p| p.date == child.date).unwrap();
-          f[pos + 1]
-        })));
-      } else {
-        set_selected_note.set(Some(notes.with(|f| {
-          let pos = f.iter().position(|p| p.date == child.date).unwrap();
-          f[pos - 1]
-        })));
+
+      match notes.read().as_slice() {
+        [_single_note] => selected_note.set(None),
+        [.., before_last_note, last_note] if last_note.date == child.date => {
+          selected_note.set(Some(before_last_note.to_owned()))
+        }
+        _ => {
+          selected_note.set(
+            notes
+              .read()
+              .windows(2)
+              .find(|window| window[0].date == child.date)
+              .map(|window| window[1].to_owned()),
+          );
+        }
       }
-      set_notes.update(|f| f.retain(|item| item.date != child.date));
+      notes.write().retain(|item| item.date != child.date);
     }
   };
   let update_selected_note_title = move |event| {
-    batch(|| {
-      set_notes.update(|notes| {
-        notes.iter().filter(|note| note.date == selected_note.get().unwrap().date).next().unwrap().title.set(event_target_value(&event));
-      }); // For Updating the local storage
-    });
-    selected_note.get().unwrap().title.set(event_target_value(&event)) // For Updating the selected item in the list
+    notes
+      .iter()
+      .filter(|note| note.read().date == selected_note.unwrap().read().date)
+      .next()
+      .unwrap()
+      .title()
+      .set(event_target_value(&event));
+
+    selected_note.unwrap().title().set(event_target_value(&event)) // For Updating the selected item in the list
   };
   let update_selected_note_content = move |event| {
-    batch(|| {
-      set_notes.update(|notes| {
-        notes.iter().filter(|note| note.date == selected_note.get().unwrap().date).next().unwrap().content.set(event_target_value(&event));
-      }); // For Updating the local storage
-    });
-    selected_note.get().unwrap().content.set(event_target_value(&event)) // For Updating the selected item in the list
+    notes
+      .iter()
+      .filter(|note| note.read().date == selected_note.unwrap().read().date)
+      .next()
+      .unwrap()
+      .content()
+      .set(event_target_value(&event));
+
+    selected_note.unwrap().content().set(event_target_value(&event)) // For Updating the selected item in the list
   };
 
   view! {
@@ -75,34 +87,76 @@ fn App() -> impl IntoView {
           "ADD NEW NOTE"
         </button>
         <ul>
-          <For each=move || notes.get() key=move |note| note.date let:child>
+          <For each=move || notes.iter() key=move |note| note.date().get() let:child>
             <li
               class="note-item new-item"
-              class:selected=move || { selected_note.with(|f| f.is_some() && f.unwrap().date == child.date) }
-              on:click=move |_| set_selected_note.set(Some(child))
+              class:selected=move || {
+                  selected_note
+                      .read()
+                      .as_ref()
+                      .is_some_and(|it| {
+                          console_log(&format!("{:#?}", it.date));
+                          console_log(&format!("{:#?}", child.read().date));
+                          console_log(
+                              &format!("{:#?}", notes.iter().map(|it| it.get()).collect::<Vec<_>>()),
+                          );
+                          it.date == child.read().date
+                      })
+              }
+
+              on:click=move |_| selected_note.set(Some(child.get()))
             >
               <div class="items">
-                <div class="title">{child.title}</div>
-                <div class="content">{child.content}</div>
+                <div class="title">{move || child.title().get()}</div>
+                <div class="content">{move || child.content().get()}</div>
               </div>
-              <button on:click=delete_note(child) class="fa fa-trash delete-button"></button>
+              <button on:click=delete_note(child.get()) class="fa fa-trash delete-button"></button>
             </li>
           </For>
         </ul>
       </div>
-      <div id="editor">
-        <Show
-          when=move || selected_note.with(|f| f.is_some())
 
-          fallback=|| {
+      <div id="editor">
+        <ShowSome
+          option=selected_note
+          let:selected_note
+          fallback=move || {
               view! { <div style="margin:auto; font-size:21px;">"Pick a note"</div> }
           }
         >
 
-          <textarea id="title-editor" rows="2" prop:value=selected_note.with(|f| f.unwrap().title) on:input=update_selected_note_title></textarea>
-          <textarea id="content-editor" prop:value=selected_note.with(|f| f.unwrap().content) on:input=update_selected_note_content></textarea>
-        </Show>
+          <textarea
+            id="title-editor"
+            rows="2"
+            prop:value=selected_note.title
+            on:input=update_selected_note_title
+          ></textarea>
+          <textarea
+            id="content-editor"
+            prop:value=selected_note.content
+            on:input=update_selected_note_content
+          ></textarea>
+        </ShowSome>
       </div>
     </div>
+  }
+}
+
+#[component]
+pub fn ShowSome<N, T, EF>(
+  children: EF,
+  #[prop(into)] option: Store<Option<T>>,
+  #[prop(optional, into)] fallback: ViewFn,
+) -> impl IntoView
+where
+  N: IntoView + 'static,
+  T: Clone + 'static + Send + Sync + PartialEq,
+  EF: Fn(T) -> N + 'static + Send + Sync,
+{
+  let memoized_when = Memo::new_owning(move |_| (option.get(), option.with(Option::is_some)));
+
+  move || match memoized_when.get() {
+    Some(value) => Either::Right(children(value).into_view()),
+    None => Either::Left(fallback.run()),
   }
 }
